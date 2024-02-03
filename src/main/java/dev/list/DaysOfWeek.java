1e0cfe310c61da696a6d6501dc8ce9a8a8cc54dd
Borja <elbarmandev@gmail.com>
2024-02-03 15:58:42 +0100

    Days of week class finish

---
+++ src/main/java/dev/list/DaysOfWeek.java
@@ -0,0 +1,52 @@
+package dev.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DaysOfWeek {
+
+    private List<String> daysOfWeek;
+
+    public DaysOfWeek() {
+        daysOfWeek = new ArrayList<>();
+        createDaysList();
+    }
+
+    private void createDaysList() {
+        daysOfWeek.add("Monday");
+        daysOfWeek.add("Tuesday");
+        daysOfWeek.add("Wednesday");
+        daysOfWeek.add("Thursday");
+        daysOfWeek.add("Friday");
+        daysOfWeek.add("Saturday");
+        daysOfWeek.add("Sunday");
+    }
+
+    public List<String> getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    public int getLength() {
+        return daysOfWeek.size();
+    }
+
+    public void removeDay(String day) {
+        daysOfWeek.remove(day);
+    }
+
+    public String getDay(int index) {
+        return daysOfWeek.get(index);
+    }
+
+    public boolean dayExists(String day) {
+        return daysOfWeek.contains(day);
+    }
+
+    public void sortAlphabetically() {
+        Collections.sort(daysOfWeek);
+    }
+
+    public void clearList() {
+        daysOfWeek.clear();
+    }
